aa3806b9480ea3f7a915daf6316574a09660073c
OllieOntime <ollie.church@ontime.co>
2026-02-28 11:36:22 +0000

    Updates socials image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://olliechurch.dev',
+  site: 'https://olliechurch.co.uk',
   vite: {
     plugins: [tailwindcss()]
   }
